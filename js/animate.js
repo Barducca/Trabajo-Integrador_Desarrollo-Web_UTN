32745f7711e59c4a17f50d8ed901f62eabd87f2f
Christian Barducca <128390408+Barducca@users.noreply.github.com>
2023-04-13 22:56:10 -0300

    Quite las animaciones del index

--- js/animate.js
+++ js/animate.js
@@ -1,46 +1,10 @@
 window.addEventListener("scroll", function(){
 
-    if(document.getElementById("index")){
-        animarIndex()
-    }
-    else if(document.getElementById("empresa")){
+    if(document.getElementById("empresa")){
         animarEmpresa()
     }
+
 });
-
-function animarIndex(){
-
-    console.log("asd");
-
-    var animarImg = document.querySelectorAll(".article-img");
-    var animarDesc = document.querySelectorAll(".descripcion");
-  
-    for (var i = 0; i < animarImg.length; i++){
-        var windowHeight = window.innerHeight;
-        var elementTop = animarImg[i].getBoundingClientRect().top;
-        var elementVisible = 100;
-  
-        if (elementTop < windowHeight - elementVisible){
-            animarImg[i].classList.add("animate__bounceInLeft");
-        } 
-        else{
-            animarImg[i].classList.remove("animate__bounceInLeft");
-        }
-    }
-
-    for (var i = 0; i < animarDesc.length; i++){
-        var windowHeight = window.innerHeight;
-        var elementTop = animarDesc[i].getBoundingClientRect().top;
-        var elementVisible = 100;
-  
-        if (elementTop < windowHeight - elementVisible){
-            animarDesc[i].classList.add("animate__bounceInRight");
-        } 
-        else{
-            animarDesc[i].classList.remove("animate__bounceInRight");
-        }
-    }
-}
 
 
 function animarEmpresa() {
@@ -64,6 +28,3 @@ function animarEmpresa() {
         }
     }
 }
-  
-
-
